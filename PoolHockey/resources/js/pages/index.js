115bc85d62888430878cb13facb2c49fdd8f3069
Samuel Dube <1489005@cstjean.qc.ca>
2019-10-09 15:45:49 -0400

    Add : Laravel support for React

--- PoolHockey/resources/js/pages/index.js
+++ PoolHockey/resources/js/pages/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
-export default class Example extends Component {
+export default class Index extends Component {
     render() {
         return (
             <div className="container">
@@ -17,8 +16,4 @@ export default class Example extends Component {
             </div>
         );
     }
-}
-
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
 }
