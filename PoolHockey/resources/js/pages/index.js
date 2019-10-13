f6e58c70e2bb99c3f8f82de63f3d631afb7a143e
Maxime Boisvert <1584942@cstjean.qc.ca>
2019-10-13 11:44:01 -0400

    Add LocalizedStrings

--- PoolHockey/resources/js/pages/index.js
+++ PoolHockey/resources/js/pages/index.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import CompteurPersonne from './comptePersonne';
+import LocalizedStrings from 'react-localization';
 
 export default class Index extends Component {
+
+    strings = new LocalizedStrings({
+        en:{
+            how:"How do you want your egg today?",
+            boiledEgg:"Boiled egg",
+            softBoiledEgg:"Soft-boiled egg",
+            choice:"How to choose the egg"
+        },
+        it: {
+            how:"Come vuoi il tuo uovo oggi?",
+            boiledEgg:"Uovo sodo",
+            softBoiledEgg:"Uovo alla coque",
+            choice:"Come scegliere l'uovo"
+        },
+        fr: {
+            how:"Comment veux-tu tes oeufs ce matin",
+            boiledEgg:"Uovo sodo",
+            softBoiledEgg:"Uovo alla coque",
+            choice:"Come scegliere l'uovo"
+        }
+    });
 
     ws = new WebSocket('ws://localhost:8090')
 
@@ -31,7 +53,7 @@ export default class Index extends Component {
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card">
-                            <div className="card-header">Example Component</div>
+                            <div className="card-header">{this.strings.how}</div>
                             <div className="card-body">I'm an example component!</div>
                         </div>
                     </div>
